e41fd3d4b7df13eb0fe9d57ab8bb6326d4014f32
alex90631 <alex90631@cs104u-2a.acad.fullerton.edu>
2019-09-14 15:54:44 -0700

    Problem 3

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
-// Sales prediction
+// Sales prediction Calculator
+
+#include <iostream>
+
+int main()
+{
+  const int TOTAL = 2103419277;
+  const double PERCENT = 0.18;
+
+  //Percentage by which sales have increased in 2103419277
+  long prediction = (TOTAL * PERCENT) + TOTAL;
+  std::cout << "Last year's sales were: $" << TOTAL << std::endl;
+  std::cout << "This year's sales prediction: $" << prediction << std::endl;
+
+    return 0;
+  }
